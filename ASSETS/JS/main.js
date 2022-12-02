8e2a0a3c6702ba5e94e0fea60f91302b27f6f1a8
Rafael Aguiar <rafaelrodriguesaguiar18@gmail.com>
2022-12-02 16:42:39 -0300

    pequenas melhorias

--- ASSETS/JS/main.js
+++ ASSETS/JS/main.js
@@ -1,7 +1,7 @@
 const cabecalholista = document.querySelectorAll(".cabecalho__item");
 const imoveis = cabecalholista[3];
 const nossoApp = cabecalholista[4];
-
+/* avisa que não esta pronto*/
 function avisa () {
     alert("Ainda em construção");
 }
